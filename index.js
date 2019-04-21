356a921533ced2451a34b652fd1896bb2226e88c
Kumar Rounak <kumarrounak@iMac.local>
2019-04-21 22:54:27 +0530

    Change 1

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello Fargate!'))
+app.get('/fargate', (req, res) => res.send('Hello Fargate!'))
 app.get('/fargatetest-service', (req, res) => res.send('Hello Fargate World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
