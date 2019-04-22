6987758d2fe38397d21ce1e50a9ffd34ba5030af
Kumar Rounak <krounak@icloud.com>
2019-04-22 11:30:22 +0530

    Test

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const app = express()
 const port = 3000
 
 app.get('/fargatetest-service-api1', (req, res) => res.send('Hello Fargate!'))
-app.get('/fargatetest-service', (req, res) => res.send('Hello Fargate World!'))
+app.get('/fargatetest-service', (req, res) => res.send('Hello Fargate World Redployed!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
